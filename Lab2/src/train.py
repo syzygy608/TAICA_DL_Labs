85296874314c748cc631dde48ad047017379a685
syzygy608 <hankwu99@gmail.com>
2025-03-17 15:19:00 +0800

    fix valid

--- Lab2/src/train.py
+++ Lab2/src/train.py
@@ -15,7 +15,7 @@ def train(args):
     # 取出訓練集和驗證集
     train_data = load_dataset(args.data_path, 'train')
     train_data_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True)
-    val_data = load_dataset(args.data_path, 'val')
+    val_data = load_dataset(args.data_path, 'valid')
     val_data_loader = DataLoader(val_data, batch_size=args.batch_size, shuffle=False)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -71,7 +71,7 @@ def train(args):
 
 def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
-    parser.add_argument('--data_path', type=str, help='path of the input data')
+    parser.add_argument('--data_path', type=str, default="../dataset", help='path of the input data')
     parser.add_argument('--epochs', '-e', type=int, default=5, help='number of epochs')
     parser.add_argument('--batch_size', '-b', type=int, default=1, help='batch size')
     parser.add_argument('--learning-rate', '-lr', type=float, default=1e-5, help='learning rate')
